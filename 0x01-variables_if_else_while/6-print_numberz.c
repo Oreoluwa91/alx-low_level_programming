3ed5b8432d28cb84c9b34c47a49927ddd8d294ec
Oreoluwa91 <emmanueloreoluwa91@gmail.com>
2023-09-08 17:30:04 -0700

    jj

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,9 +11,9 @@ int main(void)
     int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int i;
     for (i = 0; i < 10; i++)
-    {
+{
     putchar(num[i] + '0');
-    }
+}
     putchar('\n');
     return (0);
 }
